50898c3aa118acad3b7a84cb30581c736c5657e7
Jarten <Artemy2MA@outlook.com>
2025-07-30 12:04:44 -0700

    attach script

---
+++ attachable.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@export var attached: Node2D
+
+ 
+func _physics_process(delta: float) -> void:
+	const DISTANCE: float = 5000; # export on attaching later
+	var new_pos = (global_position - attached.global_position).normalized();
+	global_position = (new_pos * DISTANCE) + attached.global_position;
+	#global_rotation = new_pos.angle()
+	
