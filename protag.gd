17724aad67bdea858c7018e9838c8e2885ccc31b
Jarten <Artemy2MA@outlook.com>
2025-07-30 12:34:12 -0700

    player movement

--- protag.gd
+++ protag.gd
@@ -1,1 +1,12 @@
 extends "res://attachable.gd"
+
+func _physics_process(delta: float) -> void:
+	var input: Vector2 = Vector2.ZERO
+	if Input.is_key_pressed(KEY_A):
+		input += Vector2.LEFT.rotated(global_rotation)
+	if Input.is_key_pressed(KEY_D):
+		input += Vector2.RIGHT.rotated(global_rotation)
+	global_position += input * delta * 1000.;
+	
+	super._physics_process(delta)
+	pass
