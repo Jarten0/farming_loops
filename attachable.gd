9a1241c6f94e4861c1a18987b55a49d7730fc547
Jarten <Artemy2MA@outlook.com>
2025-07-30 12:10:47 -0700

    rotate attachments

--- attachable.gd
+++ attachable.gd
@@ -7,5 +7,6 @@ func _physics_process(delta: float) -> void:
 	const DISTANCE: float = 5000; # export on attaching later
 	var new_pos = (global_position - attached.global_position).normalized();
 	global_position = (new_pos * DISTANCE) + attached.global_position;
-	#global_rotation = new_pos.angle()
+	
+	global_rotation = (new_pos * Vector2(-1,1)).angle_to(Vector2.UP)
 	
