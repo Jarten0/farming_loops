57cf23d69c7c8aa068a04517953e668558639d42
Jarten <Artemy2MA@outlook.com>
2025-07-31 15:29:29 -0700

    sun prediction functionality

---
+++ global/prediction_sun.gd
@@ -0,0 +1,47 @@
+extends "res://planets/sun.gd"
+
+@export var base_sun: Node2D
+@export var trail_texture: Texture
+@export var max_steps: int = 1000
+@export var simuli: Array[Sprite2D] = []
+
+func reset_prediction() -> void:
+	root_node = base_sun.root_node
+	rotation_speed = base_sun.rotation_speed
+	distance = base_sun.distance
+	time = base_sun.time
+	speed = base_sun.speed
+	velocity = base_sun.velocity
+	targetted_planet = base_sun.targetted_planet
+	step = 0
+	for child in get_children():
+		child.queue_free()
+
+func _ready() -> void:
+	reset_prediction()
+	for i in range(max_steps):
+		simuli.insert(i, null)
+
+func _physics_process(delta: float) -> void:
+	
+	if base_sun.step % 10 == 0 && simuli[base_sun.step / 10]:
+		simuli[base_sun.step / 10].queue_free()
+	
+	if step >= max_steps + base_sun.step - 9:
+		return
+	
+	for i in range(10):
+		super._physics_process(delta)
+	
+	var new_sprite: Sprite2D = Sprite2D.new()
+	new_sprite.texture = trail_texture
+	new_sprite.global_position = global_position
+	new_sprite.top_level = true
+	new_sprite.scale = Vector2.ONE * 20;
+	new_sprite.z_index = (-step % 20) - 20
+	$".".add_child(new_sprite)
+	var index = (step / 10) % max_steps;
+	if simuli[index]:
+		simuli[index].queue_free()
+	
+	simuli[index] = new_sprite
--- planets/sun.gd
+++ planets/sun.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Node2D
 
 @export var root_node: Collections
 @export var rotation_speed: float = 1 / 10.
@@ -6,8 +6,11 @@ extends Area2D
 @export var time: float
 @export var speed: float
 @export var velocity: Vector2
+@export var targetted_planet: Planet
+var step = 0 # used for simulation tracking
 
 func _physics_process(delta: float) -> void:
+	step += 1; 
 	for planet in root_node.planets:
 		if planet.global_position.distance_squared_to(global_position) < pow(planet.size, 2):
 			continue
@@ -15,7 +18,24 @@ func _physics_process(delta: float) -> void:
 			var distance = (planet.global_position - global_position)
 			var grav_force = (-pow(1.1, distance.length() / 1000.) + 1000) 
 			if grav_force < 0:
+				targetted_planet = planet
 				continue
 			
 			velocity += grav_force * distance.normalized() * delta * planet.density_modifier
+	
+	const MAX_DISTANCE: float = 35_000
+	if global_position.length_squared() > pow(MAX_DISTANCE, 2):
+		velocity += -position.normalized() \
+			* max(abs(position.x) - MAX_DISTANCE, abs(position.y) - MAX_DISTANCE) \
+			/ 1. * delta;
+			
+	if targetted_planet:
+		var target_velocity = (global_position - targetted_planet.global_position) \
+			.normalized().rotated(90.) * targetted_planet.size * targetted_planet.density_modifier
+		var target_distance = targetted_planet.size * 2;
+		velocity += (target_velocity - velocity) * 0.8 * delta;
+		var distance_from_target = position.distance_to(target_distance)
+		velocity += distance_from_target * delta
+		
+	
 	position += velocity * delta
